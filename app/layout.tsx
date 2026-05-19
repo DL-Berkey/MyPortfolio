16ae89ac9d1b0e3f3ca888d67488296b801987b0
DL-Berkey <ruyria@naver.com>
2026-05-19 14:50:33 +0900

    add: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,25 @@
-import { Geist, Geist_Mono, Noto_Sans } from "next/font/google"
-
 import "./globals.css"
+
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
-
-const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+import { cn } from "@/lib/utils"
+import { pretendard } from "@/lib/font"
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", notoSans.variable)}
-    >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html
+            lang="ko"
+            suppressHydrationWarning
+            className={cn("antialiased", "font-sans", pretendard.className)}
+        >
+            <body>
+                <ThemeProvider>
+                    <div className="container mx-auto">{children}</div>
+                </ThemeProvider>
+            </body>
+        </html>
+    )
 }
---
+++ lib/font.ts
@@ -0,0 +1,5 @@
+import localFont from "next/font/local"
+
+export const pretendard = localFont({
+    src: "../public/font/Pretendard-Regular.woff2",
+})
